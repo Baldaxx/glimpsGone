ba95097df234e58cd6df02bbca3874cf7becd7d0
virginie.bm13@icloud.com <virginie.bm13@icloud.com>
2024-02-22 16:49:32 +0100

    Animation sur le titre

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ $(document).ready(function() {
     });
 });
 
+//Animation sur le titre 
+  function applyGlitchEffect() {
+    const title = document.querySelector('.titre');
+    title.classList.add('glitch');
+
+    setTimeout(() => {
+      title.classList.remove('glitch');
+    }, 1000);
+  }
+
+  function randomGlitchEffect() {
+    const minTime = 10000;
+    const maxTime = 2000; 
+    const randomTime = Math.random() * (maxTime - minTime) + minTime;
+
+    setTimeout(() => {
+      applyGlitchEffect();
+      randomGlitchEffect(); 
+    }, randomTime);
+  }
+  document.addEventListener('DOMContentLoaded', randomGlitchEffect);
+
+  
