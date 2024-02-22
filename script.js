658bacfcbd32a8fa7541829859b293baa80b3634
virginie.bm13@icloud.com <virginie.bm13@icloud.com>
2024-02-22 16:28:37 +0100

    3.2 3.3 formulaire ok

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+// menu burger
 function toggleMenu() {
     var menu = document.getElementById("menuBurgerDesign");
     menu.classList.toggle("menuShown");
 }
+
+// librairie Parsley pour validation de formulaire
+$(document).ready(function() {
+    $('.formulaireProposer').parsley();
+    Parsley.addMessages('fr', {
+        defaultMessage: "Ce champ est obligatoire.",
+    });
+    Parsley.setLocale('fr');
+
+    $('.formulaireProposer').on('submit', function(e) {
+        e.preventDefault();
+        if($(this).parsley().isValid()) {
+            window.location.href = 'proposerMerci.html'; // 
+        }
+    });
+});
+
