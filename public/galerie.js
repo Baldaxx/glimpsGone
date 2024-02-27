c8ebe621801755547287d79adc9ac2f2b35d3e33
virginie.bm13@icloud.com <virginie.bm13@icloud.com>
2024-02-27 19:21:52 +0100

    correction bouton jaime/jaimepas

--- public/galerie.js
+++ public/galerie.js
@@ -24,8 +24,8 @@ function afficheOeuvre(oeuvre) {
 let toutesLesOeuvres = [];
 let identifiantOeuvreCourante = 0; // Indice de l'oeuvre courante
 
-function initialisation() {
-  fetch("http://localhost:3000/api/oeuvres")
+function recupererEtAfficherOeuvre() {
+  fetch("/api/oeuvres")
     .then((response) => response.json())
     .then((data) => {
       toutesLesOeuvres = data;
@@ -36,54 +36,58 @@ function initialisation() {
       }
     })
     .catch((error) => console.error("Erreur :", error));
+}
 
-  // Configuration bouton j'aime
-  document.getElementById("btn_jaime").addEventListener("click", function () {
-    fetch(
-      `/api/oeuvres/${toutesLesOeuvres[identifiantOeuvreCourante].id}/jaime`,
-      { method: "POST" }
-    )
-      .then((response) => response.json())
-      .then(() => {
-        afficheOeuvre(toutesLesOeuvres[identifiantOeuvreCourante]);
-      });
-  });
+function initialisation() {
+    recupererEtAfficherOeuvre();
 
-  // Configuration bouton j'aime pas
-  document
-    .getElementById("btn_jaime_pas")
-    .addEventListener("click", function () {
-      fetch(
-        `/api/oeuvres/${toutesLesOeuvres[identifiantOeuvreCourante].id}/jaimePas`,
-        { method: "POST" }
-      )
-        .then((response) => response.json())
-        .then(() => {
-          afficheOeuvre(toutesLesOeuvres[identifiantOeuvreCourante]);
+    // Configuration bouton j'aime
+    document.getElementById("btn_jaime").addEventListener("click", function () {
+        fetch(
+            `/api/oeuvres/${toutesLesOeuvres[identifiantOeuvreCourante].id}/jaime`,
+            { method: "POST" }
+        )
+            .then((response) => response.json())
+            .then(() => {
+                recupererEtAfficherOeuvre();
+            });
+    });
+
+    // Configuration bouton j'aime pas
+    document
+        .getElementById("btn_jaime_pas")
+        .addEventListener("click", function () {
+            fetch(
+                `/api/oeuvres/${toutesLesOeuvres[identifiantOeuvreCourante].id}/jaimePas`,
+                { method: "POST" }
+            )
+                .then((response) => response.json())
+                .then(() => {
+                    recupererEtAfficherOeuvre();
+                });
         });
+
+    // Configuration bouton suivant
+    document.getElementById("btn_suivant").addEventListener("click", function () {
+        if (identifiantOeuvreCourante + 1 < toutesLesOeuvres.length) {
+            identifiantOeuvreCourante++;
+            afficheOeuvre(toutesLesOeuvres[identifiantOeuvreCourante]);
+        } else {
+            alert("Il n'y a plus d'oeuvres à voir !");
+        }
     });
 
-  // Configuration bouton suivant
-  document.getElementById("btn_suivant").addEventListener("click", function () {
-    if (identifiantOeuvreCourante + 1 < toutesLesOeuvres.length) {
-      identifiantOeuvreCourante++;
-      afficheOeuvre(toutesLesOeuvres[identifiantOeuvreCourante]);
-    } else {
-      alert("Il n'y a plus d'oeuvres à voir !");
-    }
-  });
-
-  // Configuration bouton précédent
-  document
-    .getElementById("btn_precedent")
-    .addEventListener("click", function () {
-      if (identifiantOeuvreCourante - 1 >= 0) {
-        identifiantOeuvreCourante--; 
-        afficheOeuvre(toutesLesOeuvres[identifiantOeuvreCourante]);
-      } else {
-        alert("Il n'y a plus d'oeuvres à voir !");
-      }
-    });
+    // Configuration bouton précédent
+    document
+        .getElementById("btn_precedent")
+        .addEventListener("click", function () {
+            if (identifiantOeuvreCourante - 1 >= 0) {
+                identifiantOeuvreCourante--; 
+                afficheOeuvre(toutesLesOeuvres[identifiantOeuvreCourante]);
+            } else {
+                alert("Il n'y a plus d'oeuvres à voir !");
+            }
+        });
 }
 
 window.onload = function () {
